9e7afb6a3a03147c0f977a4ef1232ac7d01f00e5
Theodore Brockman <tbrockma@ualberta.ca>
2015-08-13 16:01:35 -0600

    fix

--- angular-touch.js
+++ angular-touch.js
@@ -352,11 +352,11 @@ ngTouch.directive('ngClick', ['$parse', '$timeout', '$rootElement',
     }
 
     // If we didn't find an allowable region, bust the click.
-    event.stopPropagation();
-    event.preventDefault();
+    //event.stopPropagation();
+    //event.preventDefault();
 
     // Blur focused form elements
-    event.target && event.target.blur && event.target.blur();
+    //event.target && event.target.blur && event.target.blur();
   }
 
 
